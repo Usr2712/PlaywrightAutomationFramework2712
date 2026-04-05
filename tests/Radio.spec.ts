0a668580297672cb93f7529bcd74edaf592feccd
Sai <your email>
2026-04-05 22:45:26 +0530

    Updated

--- tests/Radio.spec.ts
+++ tests/Radio.spec.ts
@@ -8,14 +8,14 @@ test('Radio Btn1 Functionality Check @sanity', async ({ page }) => {
   expect(await radioFun.isRadioChecked('radio1')).toBeTruthy();
 });
 
-test('Radio Btn2 Functionality Check', async ({ page }) => {
+test('Radio Btn2 Functionality Check @sanity', async ({ page }) => {
   const radioFun = new radioFunctionality(page);
 
   await radioFun.selectRadio('radio2');
   expect(await radioFun.isRadioChecked('radio2')).toBeTruthy();
 });
 
-test('Radio Btn3 Functionality Check', async ({ page }) => {
+test('Radio Btn3 Functionality Check @sanity', async ({ page }) => {
   const radioFun = new radioFunctionality(page);
 
   await radioFun.selectRadio('radio3');
